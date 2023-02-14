bfa6bd817be86a47ab14bcd4f5df66682faf6186
Flanker-E <1014211757@qq.com>
2023-02-14 16:10:32 -0500

    backtrace

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -352,3 +352,12 @@ sfence_vma()
 
 typedef uint64 pte_t;
 typedef uint64 *pagetable_t; // 512 PTEs
+
+// use inline to read s0, in which stores current executing function's frame pointer
+static inline uint64
+r_fp()
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x) );
+  return x;
+}
